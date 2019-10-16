14559b252256c0da3ace5c6da085f0199e972371
Tim Izzo <tim@octree.ch>
2019-10-16 09:51:12 +0200

    Set logfile name with env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,8 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = ::Logger::Formatter.new()
+
   config.action_mailer.smtp_settings = {
     :address        => Rails.application.secrets.smtp_address,
     :port           => Rails.application.secrets.smtp_port,
@@ -92,6 +93,11 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  # Set log filename
+  logger = ActiveSupport::Logger.new("#{Rails.root}/log/#{ENV['LOG_FILENAME'] || 'production.log'}")
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
