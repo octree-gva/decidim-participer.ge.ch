692d45a483fd9eb51169c3eb688540f6530297eb
Tim Izzo <tim@octree.ch>
2019-12-16 15:20:36 +0100

    Use SSL/TLS for HTTP endpoint

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM git.octree.ch:4567/decidim/decidim-core:c51aed69
 LABEL maintainer="hello@octree.ch"
 
 ENV RAILS_ENV=production
-ENV PORT=3000
 ENV RAILS_SERVE_STATIC_FILES=true
 ENV SECRET_KEY_BASE=no_need_for_such_secrecy
 ENV TZ=Europe/Zurich
--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -22,6 +22,9 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+# HTTPS inteface
+ssl_bind '0.0.0.0', '443', { key: '/certs/privkey.pem', cert: '/certs/fullchain.pem' }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
